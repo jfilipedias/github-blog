a7e5467079fd2eb1d92dcb91f63f7539080452af
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2023-01-05 09:34:31 -0300

    Cria o hook de fetch de um post

---
+++ src/hooks/usePost.ts
@@ -0,0 +1,24 @@
+import { useApi } from './useApi'
+
+interface Post {
+  url: string
+  id: number
+  number: number
+  title: string
+  user: {
+    login: string
+  }
+  comments: number
+  created_at: string
+  body: string
+}
+
+export function usePost(id: number): Post | undefined {
+  const { response } = useApi<Post>(
+    `/repos/${import.meta.env.VITE_GITHUB_USERNAME}/${
+      import.meta.env.VITE_GITHUB_REPO
+    }/issues/${id}`,
+  )
+
+  return response
+}
